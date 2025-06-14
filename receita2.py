e87231ff8a0e195430d9e803ae40b9de81a91f51
VitoriaMoro <morovitoria1@gmail.com>
2025-06-14 18:28:39 -0300

    Update receita2.py

--- receita2.py
+++ receita2.py
@@ -209,7 +209,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
     
 )
-st.image("Exp Chef.png")
+st.image("Exp Chef.png",width=300)
 
 if 'saved_main_recipes' not in st.session_state:
     st.session_state.saved_main_recipes = []
